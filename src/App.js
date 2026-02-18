40ed8c6a310333eae86ecbba8ca3bd2906e526a7
Billy Bones <mr.fkbillybones@gmail.com>
2026-02-18 20:14:39 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './css/index.css'
+import './css/App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from "react-router-dom"
+import Home from "./components/Home";
+import FaqQ from "./components//Faq";
+import Cart from "./components/Cart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
 export default App;
+export function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<Home title="Главная" />} />
+        <Route path="/faqQ" element={<FaqQ title="Вопросы" />} />
+          <Route path="/cart" element={<Cart title="Корзина" />} />
+      </Routes>
+  )
+}
---
+++ src/components/Cart.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Cart extends Component {
+    render() {
+        return (
+            <div>
+                ПРИВЕТ МИР КОРЗИНА
+            </div>
+        );
+    }
+}
+
+export default Cart;
